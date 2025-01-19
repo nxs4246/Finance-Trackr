6ea71a4841a83e2f1b3b2ced92d0880b48a18bad
nxs4246 <nxs4246@mavs.uta.edu>
2025-01-18 23:34:01 -0600

    init setup for spring boot project

---
+++ README.md
@@ -0,0 +1,4 @@
+a personal finance tracker app
+
+Note:
+Rename "application-sample.properties" file in src\main\resources to "application.properties" and replace the environment variables in it.
---
+++ src/main/java/com/example/financetracker/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.financetracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Finance Tracker Application!";
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, this is another API endpoint!";
+    }
+
+}
