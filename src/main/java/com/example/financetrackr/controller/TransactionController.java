6477a23951577cfaaa684739f7a4f81b78413753
nxs4246 <nxs4246@mavs.uta.edu>
2025-04-06 00:12:44 -0500

    completed controller for CRUD operations

--- src/main/java/com/example/financetrackr/controller/TransactionController.java
+++ src/main/java/com/example/financetrackr/controller/TransactionController.java
@@ -5,13 +5,11 @@ import com.example.financetrackr.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/transactions")
 public class TransactionController {
 
@@ -25,13 +23,52 @@ public class TransactionController {
     @GetMapping("/list")
     public String listTransactions(Model theModel) {
 
-        return transactionService.findAll().toString();
-        //List<Transaction> theTransactions = transactionService.findAll();
+        List<Transaction> theTransactions = transactionService.findAll();
 
         // add to the spring model
-        //theModel.addAttribute("transactions", theTransactions);
+        theModel.addAttribute("transactions", theTransactions);
 
         // this file is under templates/transactions/list-transactions.html
-        //return "transactions/list-transactions";
+        return "transactions/list-transactions";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        // create model attribute to bind form data
+        Transaction theTransaction = new Transaction();
+
+        // add to the spring model
+        theModel.addAttribute("transaction", theTransaction);
+
+        return "transactions/transaction-form";
+    }
+
+    @PostMapping("/save")
+    public String saveTransaction(@ModelAttribute("transaction") Transaction theTransaction) {
+        // save the transaction
+        transactionService.save(theTransaction);
+        // use a redirect to prevent duplicate submission
+        return "redirect:/transactions/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("transactionId") int theId, Model theModel) {
+
+        // get the transaction from the service
+        Transaction theTransaction = transactionService.findById(theId);
+
+        // set employee as a model attribute to pre-populate the form
+        theModel.addAttribute("transaction", theTransaction);
+
+        // send over to our form
+        return "transactions/transaction-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("transactionId") int theId) {
+
+        transactionService.deleteById(theId);
+        return "redirect:/transactions/list";
     }
 }
--- src/main/java/com/example/financetrackr/service/TransactionService.java
+++ src/main/java/com/example/financetrackr/service/TransactionService.java
@@ -12,4 +12,5 @@ public interface TransactionService {
     Transaction save(Transaction theTransaction);
     void deleteById(int id);
 
+
 }
